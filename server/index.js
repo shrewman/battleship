60d27827bd0521601b200a7fbe86d076c325ee23
FeelsWhiteMan <feelswhiteman@gmail.com>
2023-06-29 13:38:03 +0300

    Added UML diagrams

--- server/index.js
+++ server/index.js
@@ -1,33 +1,57 @@
+const http = require('http');
+const cors = require('cors');
 const express = require('express');
+const socketUtils = require('./utils/socketUtils');
+
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const server = http.createServer(app);
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+const io = socketUtils.sio(server);
+socketUtils.connection(io);
+
+const socketIOMiddleware = (req, res, next) => {
+  req.io = io;
+  next();
+}
+
+app.use(cors());
+
+app.use("/api/v1/hello", socketIOMiddleware, (req, res) => {
+  req.io.emit("message", `Hello, ${req.originalUrl}`);
+  res.send("hello world!");
 });
 
-app.post('/data', (req, res) => {
-  res.send('Received POST request');
+const port = 8000;
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
 
-app.get('/users/:id', (req, res) => {
-  const userId = req.params.id;
-  res.send(`User ID: ${userId}`);
-});
 
-io.on('connection', (socket) => {
-  console.log('A user connected');
+// app.get('/', (req, res) => {
+//   res.send('Hello, World!');
+// });
 
-  socket.on('message', (data) => {
-    console.log('Received message:', data);
-  });
+// app.post('/data', (req, res) => {
+//   res.send('Received POST request');
+// });
 
-  socket.on('disconnect', () => {
-    console.log('A user disconnected');
-  });
-});
+// app.get('/users/:id', (req, res) => {
+//   const userId = req.params.id;
+//   res.send(`User ID: ${userId}`);
+// });
 
-http.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
-});
+// io.on('connection', (socket) => {
+//   console.log('A user connected');
+
+//   socket.on('message', (data) => {
+//     console.log('Received message:', data);
+//   });
+
+//   socket.on('disconnect', () => {
+//     console.log('A user disconnected');
+//   });
+// });
+
+// server.listen(3000, () => {
+//   console.log('Server is running on http://localhost:3000');
+// });
