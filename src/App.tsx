3c9a196b63d40132b0226107d4553af4c021157e
feelswhiteman <feelswhiteman@gmail.com>
2024-01-21 06:00:45 +0200

    Added property boardSize to gameState

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Menu from "./components/Menu";
 import Game from "./components/Game";
 
 function App() {
+    const [isStarted, setIsStarted] = useState(false);
+    const [boardSize, setBoardSize] = useState(10);
+
     const [gameState, setGameState] = useState({
-        isStarted: false,
+        isStarted,
+        boardSize,
     });
 
+    useEffect(() => {
+        setGameState({
+            isStarted,
+            boardSize,
+        });
+    }, [isStarted, boardSize]);
+
     const startGame = () => {
-        setGameState({ ...gameState, isStarted: true });
+        setIsStarted(true);
     };
 
     return (
