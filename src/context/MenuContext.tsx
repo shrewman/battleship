6abd12bad81a2b9d78543d6d312d14321d30cb45
feelswhiteman <feelswhiteman@gmail.com>
2024-01-21 18:29:38 +0200

    Created MenuContext and UseMenuContext hook

---
+++ src/context/MenuContext.tsx
@@ -0,0 +1,12 @@
+import { createContext, Dispatch, SetStateAction } from 'react';
+import { ShipCount } from '../types';
+
+type MenuContextType = {
+  boardSize: number;
+  setBoardSize: Dispatch<SetStateAction<number>>;
+  shipCount: ShipCount;
+  setShipCount: Dispatch<SetStateAction<ShipCount>>;
+};
+
+export const MenuContext = createContext<MenuContextType | null>(null);
+
