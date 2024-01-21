1dbea9378766523c1f167a97751d13191f0a462c
feelswhiteman <feelswhiteman@gmail.com>
2024-01-21 18:31:29 +0200

    ShipCount type that represents count for each size of a ship

--- src/types.d.ts
+++ src/types.d.ts
@@ -13,3 +13,11 @@ export type MenuCell = {
     position: Position;
     state: "free" | "ship";
 };
+
+type ShipCount = [
+    { size: 5; count: number },
+    { size: 4; count: number },
+    { size: 3; count: number },
+    { size: 2; count: number },
+    { size: 1; count: number }
+];
