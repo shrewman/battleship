b7dde981a52b1ea937bf3f9fc643eea509e20b2f
feelswhiteman <feelswhiteman@gmail.com>
2024-01-21 18:32:11 +0200

    Providing components with MenuContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./App.css";
 import Menu from "./components/Menu";
 import Game from "./components/Game";
+import { MenuContext } from "./context/MenuContext";
+import { ShipCount } from "./types";
 
 function App() {
     const [isStarted, setIsStarted] = useState(false);
+
     const [boardSize, setBoardSize] = useState(10);
-
-    const [gameState, setGameState] = useState({
-        isStarted,
-        boardSize,
-    });
-
-    useEffect(() => {
-        setGameState({
-            isStarted,
-            boardSize,
-        });
-    }, [isStarted, boardSize]);
+    const [shipCount, setShipCount] = useState<ShipCount>([
+        { size: 5, count: 0 },
+        { size: 4, count: 0 },
+        { size: 3, count: 0 },
+        { size: 2, count: 0 },
+        { size: 1, count: 0 },
+    ]);
 
     const startGame = () => {
         setIsStarted(true);
@@ -26,8 +24,12 @@ function App() {
     return (
         <>
             <h1>Морський бій</h1>
-            {!gameState.isStarted && <Menu />}
-            {gameState.isStarted && <Game />}
+            <MenuContext.Provider
+                value={{ boardSize, setBoardSize, shipCount, setShipCount }}
+            >
+                {!isStarted && <Menu />}
+                {isStarted && <Game />}
+            </MenuContext.Provider>
             <div>
                 <button onClick={startGame}>Start</button>
             </div>
